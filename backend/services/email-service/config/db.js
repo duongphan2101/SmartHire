3e310387cd2976a029c644b2552bde4dfe5d095f
Duong <duongphan1912@gmail.com>
2025-11-21 00:40:33 +0700

    change_value

--- backend/services/email-service/config/db.js
+++ backend/services/email-service/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI_EMAIL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 30000, 
